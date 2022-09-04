cce04e0da3ca4e75ef33701e105b404d6cea1665
oneal.oh <oneal.oh@kakaomobility.com>
2022-09-04 15:55:26 +0900

    fix: MemberRepositoryV4_2/MemberServiceV4Test

--- src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -4,6 +4,7 @@ import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
 import hello.jdbc.repository.MemberRepositoryV3;
 import hello.jdbc.repository.MemberRepositoryV4_1;
+import hello.jdbc.repository.MemberRepositoryV4_2;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -64,13 +65,15 @@ class MemberServiceV4Test {
         }
 
         @Bean
-        MemberRepositoryV4_1 memberRepositoryV4_1() {
-            return new MemberRepositoryV4_1(dataSource);
+        MemberRepository memberRepositoryV4_2() {
+//            return new MemberRepositoryV4_1(dataSource);
+            return new MemberRepositoryV4_2(dataSource);
+
         }
 
         @Bean
         MemberServiceV4 memberServiceV4() {
-            return new MemberServiceV4(memberRepositoryV4_1());
+            return new MemberServiceV4(memberRepositoryV4_2());
         }
     }
 
